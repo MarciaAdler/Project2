5f3ad90608a113b2d6b32b7c5c83d99996507178
Frank Moran <info@francisfrankmoran.com>
2020-02-17 14:40:38 -0500

    hamburger

--- public/js/news.js
+++ public/js/news.js
@@ -1,14 +1,32 @@
-//API key: b499be5fe2be429f9cd678c5202aba0d
+let today = new Date();
+        let dd = String(today.getDate()).padStart(2, '0');
+        let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        let yyyy = today.getFullYear();
+        let currentDate = yyyy + "-" + mm + "-" + dd;
+        // console.log(currentDate);
 
-var url = 'http://newsapi.org/v2/everything?' +
-          'q=Apple&' +
-          'from=2020-02-17&' +
+        $(document).ready(() => {
+
+        let url = 'http://newsapi.org/v2/everything?language=en&' +
+          'q=Coronavirus&' +
+          'from=' + currentDate + 
           'sortBy=popularity&' +
           'apiKey=b499be5fe2be429f9cd678c5202aba0d';
 
-var req = new Request(url);
+        $.ajax({
+            url: url,
+            method: "GET"
+        }).then(response => {
+            // console.log(response);
+            let articlesArray = response.articles;
+            articlesArray.forEach(article => {
 
-fetch(req)
-    .then(function(response) {
-        console.log(response.json());
-    });
+                let newsItem = $('<div class="news-item py-3 border-bottom">');
+                
+                newsItem.append(`<p class="news-item-title mb-0"><a href="${article.url}" target="_blank">${article.title}</a></p>`);
+                newsItem.append(`<p class="news-item-source mb-0">Source: ${article.source.name}</p>`);
+                $('.news-feed').append(newsItem);
+            });
+        })
+
+        });
