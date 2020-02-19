f861e58a02752de0e239bd1fad73226f04532e3e
Ellen Park <ellenpark781@gmail.com>
2020-02-18 23:27:09 -0500

    got tweets using npm twitter and filtered by search and lang

--- public/js/twitterTest.js
+++ public/js/twitterTest.js
@@ -1,12 +1,19 @@
-var settings = {
-  "url": "https://api.twitter.com/1.1/search/tweets.json?q=coronavirus",
-  "method": "GET",
-  "timeout": 0,
-  "headers": {
-    "Authorization": "Bearer AAAAAAAAAAAAAAAAAAAAABxfCgEAAAAAgxEYPBvPgVqOCb2T4Dz2r0t4QFU%3Dmp42L7d3GGF7zScntcAiKt69jvOL06r08nsLBwJYDTzT8YThqV"
-  },
-};
+var Twitter = require('twitter');
 
-$.ajax(settings).done(function (response) {
-  console.log(response);
+var client = new Twitter({
+  consumer_key: 'KtNnLCN6QzHsMDJE7O6ycjVts',
+  consumer_secret: 'VPz51xRgxknMO4vsuuq6PggSR2f9FbObXeAMrW46x6XAoZnEmN',
+  bearer_token: 'AAAAAAAAAAAAAAAAAAAAABxfCgEAAAAAgxEYPBvPgVqOCb2T4Dz2r0t4QFU%3Dmp42L7d3GGF7zScntcAiKt69jvOL06r08nsLBwJYDTzT8YThqV'
 });
+
+// finds the five of the coronavirus searches
+ 
+client.get('search/tweets', {q: 'coronavirus cases lang:en'}, function(error, tweets, response) {
+  // console.log(tweets);
+console.log(tweets.statuses[0].text)
+console.log(tweets.statuses[1].text)
+console.log(tweets.statuses[2].text)
+console.log(tweets.statuses[3].text)
+console.log(tweets.statuses[4].text)
+});
+
